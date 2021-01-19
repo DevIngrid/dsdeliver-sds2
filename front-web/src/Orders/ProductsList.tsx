b541b059c7f99ac34df4bb5fd91dcb265ce107c6
Ingrid <juliedoisj@gmail.com>
2021-01-19 04:13:55 -0300

    Finish front-web

--- front-web/src/Orders/ProductsList.tsx
+++ front-web/src/Orders/ProductsList.tsx
@@ -1,15 +1,25 @@
+import { checkIsSelected } from "./helpers";
 import ProductCard from "./ProductCard";
 import { Product } from "./types";
 
 type Props = {
     products: Product[];
+    produtosSelecionados: Product[];
+    selecionarProduto:(product: Product)=> void;
+    //onSelectProduct:(product: Product)=> void;
 }
-function ProductList({ products }:Props) {
+
+function ProductList({ products, produtosSelecionados, selecionarProduto }:Props) {
     return (
         <div className="orders-list-container">
             <div className="orders-list-items">
                 {products.map(product => (
-                    <ProductCard key={product.id} product={product} />
+                    <ProductCard 
+                    key={product.id} 
+                    product={product}
+                    onSelectProduct={selecionarProduto}
+                    isSelected={checkIsSelected(produtosSelecionados, product)}
+                     />
                 ))}
             </div>
         </div>
